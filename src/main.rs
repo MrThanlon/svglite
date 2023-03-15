f09843a4b82468c9e4722dd1a8d683840b2461ef
黄子懿 <huangziyi@canaan-creative.com>
2023-03-15 17:40:38 +0800

    update
    Signed-off-by: 黄子懿 <huangziyi@canaan-creative.com>

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use usvg::{
     NodeKind::{Group, Path, Image, Text},
     Paint::{Color, LinearGradient, RadialGradient, Pattern},
     Transform, Visibility,
-    PathSegment::{*, self}
+    PathSegment::*, NodeExt
 };
 
 struct VGLiteConfig {
@@ -18,13 +18,12 @@ struct VGLiteConfig {
     fill_rule: vg_lite_fill_t,
     /// Not used
     blend: vg_lite_blend_t,
-    quality: vg_lite_quality_t,
-    format: vg_lite_format_t
+    quality: vg_lite_quality_t
 }
 
 fn main() {
     let args: Vec<String> = args().collect();
-    if args.len() != 2 {
+    if args.len() != 5 {
         println!("Usage: {} <width> <height> <Input SVG file> <Output PNG file>", args[0]);
         return;
     }
@@ -42,8 +41,8 @@ fn main() {
         stride: 0,
         tiled: 0,
         format: vg_lite_buffer_format_VG_LITE_RGBA8888,
-        handle: unsafe { transmute([0;2]) },
-        memory: unsafe { transmute([0;2]) },
+        handle: unsafe { transmute(0usize) },
+        memory: unsafe { transmute(0usize) },
         address: 0,
         yuv: unsafe { transmute([0;18]) },
         image_mode: 0,
@@ -55,20 +54,23 @@ fn main() {
     if error != vg_lite_error_VG_LITE_SUCCESS {
         println!("error: vg_lite_allocate() failed with code {}", error);
     }
+    unsafe { vg_lite_clear(&mut buffer, transmute(0usize), 0x0000_0000) };
 
     // svg
     let svg = read_to_string(args[3].as_str()).expect("error: file not found");
     let svg = usvg::Tree::from_str(svg.as_str(), &usvg::Options::default()).expect("error: parse SVG failed");
 
+    let mut viewbox_mat = Transform::default();
+    viewbox_mat.translate(-svg.view_box.rect.left(), -svg.view_box.rect.top());
+    viewbox_mat.scale(width as f64 / svg.view_box.rect.width(), height as f64 / svg.view_box.rect.height());
+
     // dfs
-    let mut dummy = unsafe { transmute::<[u32;64],vg_lite_buffer>([0;64]) };
-    dfs(&svg.root, &mut Transform::default(), &VGLiteConfig {
-        target: &mut dummy,
-        fill_rule: 0,
-        blend: 0,
-        quality: 0,
-        format: 0
-    });
+    dfs(&svg.root, &Transform::default(), &VGLiteConfig {
+        target: &mut buffer,
+        fill_rule: vg_lite_fill_VG_LITE_FILL_NON_ZERO,
+        blend: vg_lite_blend_VG_LITE_BLEND_NONE,
+        quality: vg_lite_quality_VG_LITE_HIGH
+    }, &viewbox_mat);
     error = unsafe { vg_lite_finish() };
     if error != vg_lite_error_VG_LITE_SUCCESS {
         println!("error: vg_lite_finish() failed with code {}", error);
@@ -84,13 +86,11 @@ fn main() {
     };
 }
 
-fn dfs(node: &Node, mat: &Transform, config: &VGLiteConfig) -> u32 {
+fn dfs(node: &Node, mat: &Transform, config: &VGLiteConfig, viewbox_matrix: &Transform) -> u32 {
     match node.borrow().to_owned() {
         Group(group) => {
-            // let mut mat = mat.clone();
-            // mat.transform(&group.transform);
             for child in node.children() {
-                dfs(&child, &group.transform, config);
+                dfs(&child, &group.transform, config, viewbox_matrix);
             }
         },
         Path(path) => {
@@ -100,8 +100,34 @@ fn dfs(node: &Node, mat: &Transform, config: &VGLiteConfig) -> u32 {
             if let Some(fill) = path.fill {
                 // build path
                 let mut path_data = Vec::new();
-                let v: Vec<PathSegment>= path.data.segments().collect();
-                dbg!(v);
+                /*
+                for seg in path.data.segments() {
+                    match seg {
+                        MoveTo { x, y } => {
+                            path_data.push(2u32);
+                            path_data.push(x.round() as u32);
+                            path_data.push(y.round() as u32);
+                        },
+                        LineTo { x, y } => {
+                            path_data.push(4u32);
+                            path_data.push(x.round() as u32);
+                            path_data.push(y.round() as u32);
+                        },
+                        CurveTo { x1, y1, x2, y2, x, y } => {
+                            path_data.push(8u32);
+                            path_data.push(x1.round() as u32);
+                            path_data.push(y1.round() as u32);
+                            path_data.push(x2.round() as u32);
+                            path_data.push(y2.round() as u32);
+                            path_data.push(x.round() as u32);
+                            path_data.push(y.round() as u32);
+                        },
+                        ClosePath => {
+                            path_data.push(0u32);
+                        }
+                    }
+                }
+                */
                 for seg in path.data.segments() {
                     match seg {
                         MoveTo { x, y } => {
@@ -128,10 +154,16 @@ fn dfs(node: &Node, mat: &Transform, config: &VGLiteConfig) -> u32 {
                         }
                     }
                 }
+                let bbox = node.calculate_bbox().unwrap();
                 let mut p = vg_lite_path {
-                    bounding_box: [0., 0., unsafe { *config.target }.width as f32, unsafe { *config.target }.height as f32],
+                    bounding_box: [
+                        bbox.x() as f32,
+                        bbox.y() as f32,
+                        (bbox.x() + bbox.width()) as f32,
+                        (bbox.x() + bbox.width()) as f32
+                    ],
                     quality: config.quality,
-                    format: config.format,
+                    format: vg_lite_format_VG_LITE_FP32,
                     uploaded: unsafe { transmute::<[u32;8], vg_lite_hw_memory>([0;8]) },
                     path_length: (path_data.len() * 4) as i32,
                     path: path_data.as_mut_ptr() as *mut c_void,
@@ -145,14 +177,18 @@ fn dfs(node: &Node, mat: &Transform, config: &VGLiteConfig) -> u32 {
                         c |= (color.red as u32) << 16;
                         c |= (color.green as u32) << 8;
                         c |= (color.blue as u32) << 0;
+
+                        let mut mat2 = viewbox_matrix.clone();
+                        mat2.transform(mat);
+
                         // TODO: return error
                         let error = unsafe { vg_lite_draw(
                             config.target,
                             &mut p,
                             config.fill_rule,
                             &mut vg_lite_matrix {
-                                m: [[mat.a as f32, mat.c as f32, mat.e as f32],
-                                    [mat.b as f32, mat.d as f32, mat.f as f32],
+                                m: [[mat2.a as f32, mat2.c as f32, mat2.e as f32],
+                                    [mat2.b as f32, mat2.d as f32, mat2.f as f32],
                                     [0., 0., 1.]
                                 ]
                             },
