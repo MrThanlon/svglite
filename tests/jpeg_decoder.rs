85f7703f2cc38570ccdf31e764f431a91df9ffcb
黄子懿 <huangziyi@canaan-creative.com>
2023-03-31 10:25:35 +0800

    update
    Signed-off-by: 黄子懿 <huangziyi@canaan-creative.com>

--- tests/jpeg_decoder.rs
+++ tests/jpeg_decoder.rs
@@ -4,11 +4,11 @@ use std::io::{BufReader, Write};
 
 #[test]
 fn jpeg_decode() {
-    let file = File::open("case/github-mark.jpg").expect("failed to open file");
+    let file = File::open("case/shinonome.jpg").expect("failed to open file");
     let mut decoder = Decoder::new(BufReader::new(file));
     let pixels = decoder.decode().expect("failed to decode image");
     let metadata = decoder.info().unwrap();
     dbg!(metadata);
-    let mut file = File::create("case/github-mark.bin").expect("failed to open file");
+    let mut file = File::create("case/shinonome.bin").expect("failed to open file");
     dbg!(file.write(&pixels).unwrap());
 }
